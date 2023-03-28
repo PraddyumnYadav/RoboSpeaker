d4c70b7997af94d770e47c123031c470bf1e644d
Praddyumn Yadav <praddyumny@gmail.com>
2023-03-28 11:47:37 +0530

    Create the Project in main.py file.

---
+++ main.py
@@ -0,0 +1,22 @@
+# Importing Essentials
+from gtts import gTTS # for converting text to speech
+from playsound import playsound # for Playing Converted Audio
+
+
+# Get Text as Input from the User.
+print("Please Enter the Text Which You Want this Programm to Play(Don't Include any Punctuation).")
+text = input(":- ")
+
+# Selecting Language in Which We want to convert the Audio.
+language = "en"
+
+
+# Specifying Speed, Text and Language of the Audio and Generating it.
+audio = gTTS(text=text, lang=language, slow=False, tld="com")
+
+# Saving the Audio as audio.mp3
+audio.save("audio.mp3")
+
+
+# Playing the Converted Audio
+playsound("audio.mp3")
