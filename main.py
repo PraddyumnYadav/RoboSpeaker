6278751e7722934c1241d64a89ebb202a11e15ad
Praddyumn Yadav <praddyumny@gmail.com>
2023-03-28 12:13:28 +0530

    Organise Our Project.

--- main.py
+++ main.py
@@ -2,23 +2,27 @@
 from gtts import gTTS # for converting text to speech
 from playsound import playsound # for Playing Converted Audio
 
-# Print Welcome Message.
-print("--------------------- Welcome to RoboSpeaker -----------------------")
+def main():
+	# Print Welcome Message.
+	print("--------------------- Welcome to RoboSpeaker -----------------------")
 
-# Get Text as Input from the User.
-print("Please Enter the Text Which You Want this Programm to Play(Don't Include any Punctuation).")
-text = input(":- ")
+	# Get Text as Input from the User.
+	print("Please Enter the Text Which You Want this Programm to Play(Don't Include any Punctuation).")
+	text = input(":- ")
 
-# Selecting Language in Which We want to convert the Audio.
-language = "en"
-
-
-# Specifying Speed, Text and Language of the Audio and Generating it.
-audio = gTTS(text=text, lang=language, slow=False, tld="com")
-
-# Saving the Audio as audio.mp3
-audio.save("audio.mp3")
+	# Selecting Language in Which We want to convert the Audio.
+	language = "en"
 
 
-# Playing the Converted Audio
-playsound("audio.mp3")
+	# Specifying Speed, Text and Language of the Audio and Generating it.
+	audio = gTTS(text=text, lang=language, slow=False, tld="com")
+
+	# Saving the Audio as audio.mp3
+	audio.save("audio.mp3")
+
+
+	# Playing the Converted Audio
+	playsound("audio.mp3")
+
+if __name__ == "__main__":
+	main()
