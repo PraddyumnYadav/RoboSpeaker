67bdd14735de65ff01fb148247742b56d2cf0879
Praddyumn Yadav <praddyumny@gmail.com>
2023-03-28 21:23:35 +0530

    Make our Programm able to run in a Loop.

--- main.py
+++ main.py
@@ -1,28 +1,64 @@
-# Importing Essentials
+# Importing Essentils
+import time # for using time.sleep()
 from gtts import gTTS # for converting text to speech
-from playsound import playsound # for Playing Converted Audio
+import pygame # for playing the converted audio
+import soundfile as sf # for getting the length of the audio
 
+
+
+# Creating Main Programm
 def main():
 	# Print Welcome Message.
 	print("--------------------- Welcome to RoboSpeaker -----------------------")
+	print("Press 'q' to quit.")
 
-	# Get Text as Input from the User.
-	print("Please Enter the Text Which You Want this Programm to Play(Don't Include any Punctuation).")
-	text = input(":- ")
+	while True:
+		# Get Text as Input from the User.
+		print("Please Enter the Text Which You Want this Programm to Play(Don't Include any Punctuation).")
+		text = input(":- ")
 
-	# Selecting Language in Which We want to convert the Audio.
-	language = "en"
+		# Selecting Language in Which We want to convert the Audio.
+		language = "en"
+
+		# Exit the Programm if input is q
+		if text == "q":
+			break
+
+		# Runnign Speak Function
+		speak(text, language)
 
 
-	# Specifying Speed, Text and Language of the Audio and Generating it.
-	audio = gTTS(text=text, lang=language, slow=False, tld="com")
+# Creating speak Function
+def speak(text, language):
+    # Convert text to speech using gTTS
+    audio = gTTS(text=text, lang=language, slow=False)
 
-	# Saving the Audio as audio.mp3
-	audio.save("audio.mp3")
+    # Save the audio to a file
+    audio_file = "audio.mp3"
+    audio.save(audio_file)
+
+    # Initialize pygame
+    pygame.init()
+
+    # Load the audio file and play it
+    pygame.mixer.music.load(audio_file)
+    pygame.mixer.music.play()
+
+    # Stop Time for Audio Length
+    time.sleep(get_audio_length(audio_file))
+
+    # stop the music
+    pygame.mixer.music.stop()
+
+    # Quit pygame
+    pygame.quit()
 
 
-	# Playing the Converted Audio
-	playsound("audio.mp3")
+# Create a Function for Checking Length of Audio
+def get_audio_length(filename):
+    with sf.SoundFile(filename) as f:
+        return len(f) / f.samplerate
 
-if __name__ == "__main__":
-	main()
+
+# Run the main Function
+main()
