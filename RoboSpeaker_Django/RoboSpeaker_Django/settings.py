d60f898064d871b552327fd7b92e6f557c915353
Praddyumn Yadav <praddyumny@gmail.com>
2023-04-01 08:12:57 +0530

    Add Static Files Directory in Django.

--- RoboSpeaker_Django/RoboSpeaker_Django/settings.py
+++ RoboSpeaker_Django/RoboSpeaker_Django/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Added by Praddyumn Yadav
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'robospeaker_app', 'static')]
