1278bde0d7da69a1616fde8f59cb296f65f6ac21
Praddyumn Yadav <praddyumny@gmail.com>
2023-04-09 00:36:30 +0530

    Add Audio Player In Home screen.

--- RoboSpeaker_Django/robospeaker_app/views.py
+++ RoboSpeaker_Django/robospeaker_app/views.py
@@ -3,13 +3,11 @@ from django.http import FileResponse
 from gtts import gTTS
 import os
 
+
 def convert_text(request):
-    if request.method == 'POST':
-        text = request.POST.get('text')
-        language = "en"
-        audio = gTTS(text=text, lang=language, slow=False)
-        audio_file = os.path.join('robospeaker_app', 'static', 'audio', 'audio.mp3')
-        audio.save(audio_file)
-        return FileResponse(open(audio_file, 'rb'))
-    else:
-        return render(request, 'index.html')
+    text = request.POST.get("text")
+    language = "en"
+    audio = gTTS(text=text, lang=language, slow=False)
+    audio_file = os.path.join("robospeaker_app", "static", "audio", "audio.mp3")
+    audio.save(audio_file)
+    return render(request, "index.html")
